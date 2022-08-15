9044069be52b6bd6ae7b81681f7c23c256e4c361
Kamireddy Mahitha <107293542+mahimahitha008@users.noreply.github.com>
2022-08-15 12:44:17 +0530

    Function Component

---
+++ Function Component/App.js
@@ -0,0 +1,10 @@
+import App1 from './App1'
+let App=()=>{
+    return <div>
+        <h1>Hello,</h1>
+        <h2>Good</h2>
+        <h3>Morning!</h3>
+        <App1/>
+    </div>
+}
+export default App;
---
+++ Function Component/App1.js
@@ -0,0 +1,4 @@
+function App1(){
+    return <h1>Welcome to React Js!</h1>
+}
+export default App1;
