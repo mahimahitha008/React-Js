2d52b64859d35399a40e15371810eb54a83996b3
Kamireddy Mahitha <107293542+mahimahitha008@users.noreply.github.com>
2022-08-15 12:59:09 +0530

    Function

---
+++ Function/Func.js
@@ -0,0 +1,8 @@
+function Func(){
+    return <div>
+        <h1>React JS</h1>
+        <h2>Is using </h2>
+        <h3>Extensively.</h3>
+    </div>
+}
+export default Func;
---
+++ Function/index.js
@@ -0,0 +1,3 @@
+import Js from 'react-dom'
+import Func from './Func'
+Js.render(<Func/>,document.getElementById("Start"))
