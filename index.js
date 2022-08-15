d684d41a02e592ee2ca40878c884af1da1a9b4cc
Kamireddy Mahitha <107293542+mahimahitha008@users.noreply.github.com>
2022-08-15 12:23:32 +0530

    React

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-import js from 'react-dom'
-js.render(<h1>Hello World!</h1>,document.getElementById('Start'))
+import Js from 'react-dom'
+//Js.render("Hello World!",document.getElementById("Start"))
+//Js.render(<h1>Hello World!</h1>,document.getElementById("Start"))
+//Js.render(<div><h1>Hello</h1><h2>World!</h2></div>,document.getElementById("Start"))
+Js.render(<div><h1>Hello</h1><hr></hr><h2>World!</h2></div>,document.getElementById("Start"))
